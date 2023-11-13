39f65c04fa10caa0d31cde5e8213b04abeea22b3
bahiatrading <bahiatrading2017@gmail.com>
2023-11-13 09:35:59 -0500

    13-11 09:35

--- Client/Servicios/Implementacion/TrabajadorService.cs
+++ Client/Servicios/Implementacion/TrabajadorService.cs
@@ -1,18 +1,11 @@
 ﻿using QHSE.Client.Utilidades;
-<<<<<<< HEAD
-=======
 using SistemaPedidos.Client.Utilidades;
 using System.Net.Http.Headers;
->>>>>>> 2e36e4e72a9ceccf3d9d1b2d4fac05d43955b108
 using System.Net.Http.Json;
 
 namespace QHSE.Client.Servicios.Implementacion
 {
-<<<<<<< HEAD
-    public class TrabajadorService:ITrabajadorService
-=======
     public class TrabajadorService : ITrabajadorService
->>>>>>> 2e36e4e72a9ceccf3d9d1b2d4fac05d43955b108
     {
         private readonly HttpClient _http;
         private readonly AppData _appData;
@@ -21,7 +14,6 @@ namespace QHSE.Client.Servicios.Implementacion
         {
             _http = http;
             _appData = appData;
-<<<<<<< HEAD
         }
 
         public async Task<ResponseDTO<CreacionDTO>> Crear(CreacionDTO entidad)
@@ -44,20 +36,14 @@ namespace QHSE.Client.Servicios.Implementacion
             var result = await _http.DeleteAsync($"api/trabajador/Eliminar/{id}");
             var response = await result.Content.ReadFromJsonAsync<ResponseDTO<string>>();
             return response!.status;
-=======
             _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _appData.usuarioToken);
->>>>>>> 2e36e4e72a9ceccf3d9d1b2d4fac05d43955b108
         }
 
         public async Task<ResponseDTO<List<TrabajadorDTO>>> Lista()
         {
-<<<<<<< HEAD
             var result = await _http.GetFromJsonAsync<ResponseDTO<List<TrabajadorDTO>>>("api/trabajador/Lista");
             return result!;
-=======
-            var result = await _http.GetFromJsonAsync<ResponseDTO<List<TrabajadorDTO>>>("api/persona/Lista");
-            return result;
->>>>>>> 2e36e4e72a9ceccf3d9d1b2d4fac05d43955b108
+
         }
     }
 }
--- Server/Controllers/TrabajadorController.cs
+++ Server/Controllers/TrabajadorController.cs
@@ -1,12 +1,137 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QHSE.Server.Models;
+using QHSE.Server.Repositorio.Contrato;
+using QHSE.Shared;
 
 namespace QHSE.Server.Controllers
 {
-    public class TrabajadorController : Controller
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class TrabajadorController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IMapper _mapper;
+        private readonly ITrabajadorRepositorio _trabajadorRepositorio;
+
+        public TrabajadorController(ITrabajadorRepositorio articuloRepositorio, IMapper mapper)
         {
-            return View();
+            _mapper = mapper;
+            _trabajadorRepositorio = articuloRepositorio;
         }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            ResponseDTO<List<TrabajadorDTO>> _response = new ResponseDTO<List<TrabajadorDTO>>();
+
+            try
+            {
+                List<TrabajadorDTO> _listaTrabajadors = new List<TrabajadorDTO>();
+
+                IQueryable<Trabajador> query = await _trabajadorRepositorio.Consultar();
+                query = query.Include(c => c.IdCreateNavigation)
+                        .Where(c => c.IdCreateNavigation.Activo == 1);
+
+                _listaTrabajadors = _mapper.Map<List<TrabajadorDTO>>(query.ToList());
+
+                if (_listaTrabajadors.Count > 0)
+                    _response = new ResponseDTO<List<TrabajadorDTO>>() { status = true, msg = "ok", value = _listaTrabajadors };
+                else
+                    _response = new ResponseDTO<List<TrabajadorDTO>>() { status = false, msg = "sin resultados", value = null };
+
+                return StatusCode(StatusCodes.Status200OK, _response);
+            }
+            catch (Exception ex)
+            {
+
+                _response = new ResponseDTO<List<TrabajadorDTO>>() { status = false, msg = ex.Message, value = null };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<IActionResult> Guardar([FromBody] CreacionDTO request)
+        {
+
+            ResponseDTO<CreacionDTO> _ResponseDTO = new ResponseDTO<CreacionDTO>();
+
+            try
+            {
+                Creacion _Trabajador = _mapper.Map<Creacion>(request);
+
+                Creacion _TrabajadorCreado = await _trabajadorRepositorio.Crear(_Trabajador);
+
+
+                if (_TrabajadorCreado.IdCreate != 0)
+                    _ResponseDTO = new ResponseDTO<CreacionDTO>() { status = true, msg = "ok", value = _mapper.Map<CreacionDTO>(_TrabajadorCreado) };
+                else
+                    _ResponseDTO = new ResponseDTO<CreacionDTO>() { status = false, msg = "No se pudo crear el vendedor" };
+
+                return StatusCode(StatusCodes.Status200OK, _ResponseDTO);
+
+            }
+            catch (Exception ex)
+            {
+
+                _ResponseDTO = new ResponseDTO<CreacionDTO>() { status = false, msg = ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _ResponseDTO);
+            }
+
+
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] CreacionDTO request)
+        {
+
+            ResponseDTO<bool> _ResponseDTO = new ResponseDTO<bool>();
+
+            try
+            {
+                Creacion _Trabajador = _mapper.Map<Creacion>(request);
+                Creacion _TrabajadorEditar = await _trabajadorRepositorio.Obtener(u => u.IdCreate == _Trabajador.IdCreate);
+
+                if (_TrabajadorEditar.IdCreate != null)
+                {
+
+                    _TrabajadorEditar.FechaModi = DateTime.Now;
+                    _TrabajadorEditar.UsuaModi = _Trabajador.UsuaModi;
+                    _TrabajadorEditar.PcModi = _Trabajador.PcModi;
+                    _TrabajadorEditar.Trabajadors = _Trabajador.Trabajadors;
+
+                    bool respuesta = await _trabajadorRepositorio.Editar(_TrabajadorEditar);
+
+                    if (respuesta)
+                        _ResponseDTO = new ResponseDTO<bool>() { status = true, msg = "ok", value = true };
+                    else
+                        _ResponseDTO = new ResponseDTO<bool>() { status = false, msg = "No se pudo crear el vendedor" };
+                }
+                else
+                {
+                    _ResponseDTO = new ResponseDTO<bool>() { status = false, msg = "No se encontró el vendedor" };
+                }
+
+                return StatusCode(StatusCodes.Status200OK, _ResponseDTO);
+
+            }
+            catch (Exception ex)
+            {
+
+                _ResponseDTO = new ResponseDTO<bool>() { status = false, msg = ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _ResponseDTO);
+            }
+
+
+        }
+
+
+
     }
 }
