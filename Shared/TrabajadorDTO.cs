201ff84b97e008405dadce4de6e7e4f469d17232
ProgramadorFastdye <programador.fastdye@gmail.com>
2023-11-13 17:29:54 -0500

    17:29

--- Shared/TrabajadorDTO.cs
+++ Shared/TrabajadorDTO.cs
@@ -2,34 +2,24 @@
 {
     public class TrabajadorDTO
     {
-        public int IdVendedor { get; set; }
+        public int IdTraba { get; set; }
 
-        public int? IdTraba { get; set; }
 
-        public int? IdCargo { get; set; }
+        public string? NomTraba { get; set; }
 
-        public string? Detalles { get; set; }
+      
+        public string? ApeTraba { get; set; }
 
-        public string? TipoCargo { get; set; }
+ 
+        public string? NroDoc { get; set; }
 
-        public string? Nombres { get; set; }
+  
+        public string? NroTelefono { get; set; }
+
+     
+        public string? CorreoTraba { get; set; }
+
 
         public int? IdCreate { get; set; }
-
-        public string? UsuaCrea { get; set; }
-
-        public string? UsuaModi { get; set; }
-
-        public DateTime? FechaCrea { get; set; }
-
-        public DateTime? FechaModi { get; set; }
-
-        public string? PcCrea { get; set; }
-
-        public string? PcModi { get; set; }
-
-        public string? Correo { get; set; }
-
-        public int? IdTipoPerso { get; set; }
     }
 }
