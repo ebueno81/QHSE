82d5e6f1a81da4a44c77bf4acff9d213f5009659
bahiatrading <bahiatrading2017@gmail.com>
2023-11-13 09:22:55 -0500

    13-09 09:22

--- Client/Servicios/Contrato/ITrabajadorService.cs
+++ Client/Servicios/Contrato/ITrabajadorService.cs
@@ -2,5 +2,9 @@
 {
     public interface ITrabajadorService
     {
+        Task<ResponseDTO<List<TrabajadorDTO>>> Lista();
+        Task<ResponseDTO<CreacionDTO>> Crear(CreacionDTO entidad);
+        Task<bool> Editar(CreacionDTO entidad);
+        Task<bool> Eliminar(int id);
     }
 }
--- Client/Servicios/Implementacion/TrabajadorService.cs
+++ Client/Servicios/Implementacion/TrabajadorService.cs
@@ -1,6 +1,45 @@
-﻿namespace QHSE.Client.Servicios.Implementacion
+﻿using QHSE.Client.Utilidades;
+using System.Net.Http.Json;
+
+namespace QHSE.Client.Servicios.Implementacion
 {
-    public class TrabajadorService
+    public class TrabajadorService:ITrabajadorService
     {
+        private readonly HttpClient _http;
+        private readonly AppData _appData;
+
+        public TrabajadorService(HttpClient http, AppData appData)
+        {
+            _http = http;
+            _appData = appData;
+        }
+
+        public async Task<ResponseDTO<CreacionDTO>> Crear(CreacionDTO entidad)
+        {
+            var result = await _http.PostAsJsonAsync("api/trabajador/Guardar", entidad);
+            var response = await result.Content.ReadFromJsonAsync<ResponseDTO<CreacionDTO>>();
+            return response!;
+        }
+
+        public async Task<bool> Editar(CreacionDTO entidad)
+        {
+            var result = await _http.PutAsJsonAsync("api/trabajador/Editar", entidad);
+            var response = await result.Content.ReadFromJsonAsync<ResponseDTO<bool>>();
+
+            return response!.status;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var result = await _http.DeleteAsync($"api/trabajador/Eliminar/{id}");
+            var response = await result.Content.ReadFromJsonAsync<ResponseDTO<string>>();
+            return response!.status;
+        }
+
+        public async Task<ResponseDTO<List<TrabajadorDTO>>> Lista()
+        {
+            var result = await _http.GetFromJsonAsync<ResponseDTO<List<TrabajadorDTO>>>("api/trabajador/Lista");
+            return result!;
+        }
     }
 }
