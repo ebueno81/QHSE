78ef47d18f6585d4724b81f8dbf85ce8da1315d9
ProgramadorFastdye <programador.fastdye@gmail.com>
2023-11-14 15:48:31 -0500

    15:48

---
+++ Shared/EmpresaDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QHSE.Shared
+{
+    public class EmpresaDTO
+    {
+        public int IdEmp { get; set; }
+
+        public string? RucEmp { get; set; }
+
+        public string? RazEmp { get; set; }
+
+        public string? DirecEmp { get; set; }
+
+        public string? NroTelefono { get; set; }
+        
+        public string? CorreoEmp { get; set; }
+
+        public string? DptEmp { get; set; }
+
+        public string? ProvEmp { get; set; }
+
+        public string? DistEmp { get; set; }
+    }
+}
